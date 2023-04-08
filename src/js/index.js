97cd684bf2b16ce1092db66e8cd78e1c8f3a4c7b
nacho <nacholeon30@hotmail.com>
2023-04-08 17:27:07 -0300

    create nav and hero section

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import '../scss/styles.scss'
+import * as bootstrap from 'bootstrap'
+import nukaColaLogo from '../assets/nuka-cola-2-300w.png'
+import nukaColaBottle from '../assets/bottle-261w.png'
+
+document.querySelector('#nuka-cola-logo').src = nukaColaLogo
+document.querySelector('#nuka-cola-bottle').src = nukaColaBottle
