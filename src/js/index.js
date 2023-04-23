f4d30be02e42194fdbe6da796a243846e0410302
nacho <nacholeon30@hotmail.com>
2023-04-23 15:49:13 -0300

    add last call to action and footer

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,8 @@ import '../scss/styles.scss'
 import * as bootstrap from 'bootstrap'
 import nukaColaLogo from '../assets/nuka-cola-2-300w.png'
 import nukaColaBottle from '../assets/bottle-261w.png'
+import vaultBoy from '../assets/vault-boy.png'
 
 document.querySelector('#nuka-cola-logo').src = nukaColaLogo
 document.querySelector('#nuka-cola-bottle').src = nukaColaBottle
+document.querySelector('#vault-boy').src = vaultBoy
